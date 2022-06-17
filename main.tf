dcab1b923f1945f111bb5703b7bcc7f1e1266e89
Ebrech <ebrechsage@gmail.com>
2022-06-16 19:02:05 -0700

    Changed Main.tf, added security group

--- main.tf
+++ main.tf
@@ -1,12 +1,29 @@
 provider "aws" {
-  region  = "us-west-2"
-}
+  region  = "us-east-1"
+ }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-40d28157"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  
+  user_data = <<-EOF
+  		#!/bin/bash
+  		echo "Hello, World" > index.html
+  		nohup busybox httpd -f -p 8080 &
+  		EOF
   tags = {
     Name = "terraform-example"
+  }
+ }
+ 
+ resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
  }
