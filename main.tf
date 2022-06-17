157d9a07122c688663e6286f9a98fb7448d384f2
Ebrech <ebrechsage@gmail.com>
2022-06-16 22:55:00 -0700

    Added AZ zones and Autoscaling

--- main.tf
+++ main.tf
@@ -2,22 +2,22 @@ provider "aws" {
   region  = "us-east-1"
  }
 
-resource "aws_instance" "app_server" {
+resource "aws_launch_configuration" "app_server" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  security_groups = ["${aws_security_group.instance.id}"]
   
   user_data = <<-EOF
   		#!/bin/bash
   		echo "Hello, World" > index.html
   		nohup busybox httpd -f -p "${var.server_port}" &
   		EOF
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+  create_before_destroy = true
   }
  }
  
- resource "aws_security_group" "instance" {
+resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   
   ingress {
@@ -25,6 +25,24 @@ resource "aws_instance" "app_server" {
     to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  lifecycle {
+  create_before_destroy = true
+  }
+ }
+ 
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  
+  min_size = 2
+  max_size = 10
+  
+ tag {
+   key 			= "Name"
+   value 		= "terraform-asg-example"
+   propagate_at_launch 	= true
   }
  }
  
@@ -35,6 +53,5 @@ variable "server_port" {
 
 output "public_ip" {
   value = "${aws_instance.app_server.public_ip}"  
-  
  }  
   
